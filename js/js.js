57963e1bc7c16ef99a7c6be4a8a90918f0c8a004
Dima Dolgoter <dolgoterd@gmail.com>
2017-08-10 17:12:53 +0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,6 +1,30 @@
 $(function(){
  $(".element").typed({
           //  strings: ["<!DOCTYPE html>^1000 \n <html ^1000 \n <head> \n"],
+            stringsElement: $('#fps'),
+             startDelay: 0,
+            // backspacing speed
+            typeSpeed: 20,
+            // time before typing starts
+             backSpeed: 0,
+             // shuffle the strings
+            backDelay: 5000,
+            // Delay remove 5 sec 
+            loop: false,
+            // false = infinite
+            //loopCount: 2,
+            // show cursor
+            contentType: 'text',
+       showCursor: true,
+            // character for cursor
+            cursorChar: "<i class='fa fa-italic'></i>",
+            // attribute to type (null == text)
+     contentType: 'html'
+        });
+    });
+$(function(){
+ $(".fp").typed({
+          //  strings: ["<!DOCTYPE html>^1000 \n <html ^1000 \n <head> \n"],
             stringsElement: $('#typed-strings'),
              startDelay: 0,
             // backspacing speed
@@ -22,7 +46,6 @@ $(function(){
      contentType: 'html'
         });
     });
-
 /*плавный скролл*/
 $('a[href^="#"]').bind('click.smoothscroll',function (e) {
  e.preventDefault();
